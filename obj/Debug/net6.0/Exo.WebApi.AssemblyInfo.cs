7eeaa75396a7020a61971c36ff2df856d3ca17ad
airton07 <airtonbrito81@gmail.com>
2026-05-02 19:40:17 -0300

    valendo

--- obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/Exo.WebApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef48cba235de3c847d9399cbd2c262140e3ef3bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+800602099c93e253e3690b385ed326d274562681")]
 [assembly: System.Reflection.AssemblyProductAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Exo.WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
